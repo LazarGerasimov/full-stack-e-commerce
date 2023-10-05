26b3afb6754fea5f5dbf7702fae085991cd7ef43
Laz <lazgerasimov@gmail.com>
2023-10-05 13:32:50 +0100

    Install, setup, mount & test Clerk authentication

--- admin-dashboard/app/(root)/page.tsx
+++ admin-dashboard/app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
     return (
       <div className="p-4">
-        <Button>Click me</Button>
+        <Button variant="destructive">Click me</Button>
       </div>
     )
   }
