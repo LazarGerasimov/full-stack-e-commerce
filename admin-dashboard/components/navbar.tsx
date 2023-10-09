fbebc8d92e70b0b5bd31c30b80ca7e1a49d2f569
Laz <lazgerasimov@gmail.com>
2023-10-09 18:54:59 +0100

    Create Navbar comp scaffold

---
+++ admin-dashboard/components/navbar.tsx
@@ -0,0 +1,22 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+const Navbar = () => {
+    return (
+        <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+                <div>
+                    Store Switcher
+                </div>
+                <div>
+                    Routes
+                </div>
+                <div className="ml-auto flex items-center space-x-4">
+                    <UserButton />
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
