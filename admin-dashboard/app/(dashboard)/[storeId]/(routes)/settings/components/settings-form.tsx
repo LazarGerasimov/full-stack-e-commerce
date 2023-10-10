3ea8fbb77c2265d7f09bfd3731f4914c08d129ab
Laz <lazgerasimov@gmail.com>
2023-10-10 20:08:35 +0100

    Update: - Heading comp styles - add icons

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -3,6 +3,8 @@
 import { Store } from "@prisma/client";
 
 import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -10,12 +12,23 @@ interface SettingsFormProps {
 
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     return (
-        <div className="flex items-center justify-between">
-            <Heading
-                title="Settings"
-                description="Manage store preferences"
-            />
-        </div>
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title="Settings"
+                    description="Manage store preferences"
+                />
+                <Button
+                    variant="destructive"
+                    size="icon"
+                    onClick={() => { }}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+
+            
+        </>
     )
 }
 
--- admin-dashboard/components/ui/heading.tsx
+++ admin-dashboard/components/ui/heading.tsx
@@ -8,8 +8,10 @@ interface HeadingProps {
 const Heading: React.FC<HeadingProps> = ({ title, description }) => {
     return (
         <div>
-            <h2>{title}</h2>
-            <p>
+            <h2 className="text-3xl font-bold tracking-tight">
+                {title}
+            </h2>
+            <p className="text-sm text-muted-foreground">
                 {description}
             </p>
         </div>
