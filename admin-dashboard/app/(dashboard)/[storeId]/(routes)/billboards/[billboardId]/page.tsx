1cfce0decea762a5fc7b1ee6a696dd7ddee16001
Laz <lazgerasimov@gmail.com>
2023-10-15 09:34:24 +0100

    add BillboardPage scaffold (billboard form)

---
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,15 @@
+
+
+// Extract params from here as this is a server component
+
+const BillboardPage = async ({
+    params
+}: {
+    params: { billboardId: string }
+}) => {
+    return (
+        <div>Billboards Form</div>
+    )
+}
+
+export default BillboardPage;
