44accde9d2ac27ad00069d229f0c3f51c71b87e8
Laz <lazgerasimov@gmail.com>
2023-10-15 12:33:35 +0100

    add billboard-form

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -2,13 +2,27 @@
 
 // Extract params from here as this is a server component
 
+import prismadb from "@/lib/prismadb";
+import BillboardForm from "./components/billboard-form";
+
 const BillboardPage = async ({
     params
 }: {
     params: { billboardId: string }
 }) => {
+
+    const billboard = await prismadb.billboard.findUnique({
+        where: {
+            id: params.billboardId
+        }
+    });
+
     return (
-        <div>Billboards Form</div>
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardForm initialData={billboard} />
+            </div>
+        </div>
     )
 }
 
