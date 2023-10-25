cac7cb25cdb0652a60d248b9ba1ef5842227ee8e
Laz <lazgerasimov@gmail.com>
2023-10-25 19:06:28 +0100

    update Colors data table actions redirection routes

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
@@ -25,18 +25,18 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
     const onCopy = (id: string) => {
         navigator.clipboard.writeText(id);
-        toast.success("Size ID copied to the clipboard.");
+        toast.success("Color ID copied to the clipboard.");
     }
 
     const onDelete = async () => {
         try {
             setLoading(true);
-            await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+            await axios.delete(`/api/${params.storeId}/colors/${data.id}`);
             router.refresh();
-            toast.success("Size deleted.");
+            toast.success("Color deleted.");
         } catch (error) {
             console.log(error);
-            toast.error("Make sure you removed all products using this size first.");
+            toast.error("Make sure you removed all products using this color first.");
         } finally {
             setLoading(false);
             setOpen(false);
@@ -66,7 +66,7 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
                         <Copy className="mr-2 h-4 w-4" />
                         Copy Id
                     </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}>
+                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/colors/${data.id}`)}>
                         <Edit className="mr-2 h-4 w-4" />
                         Update
                     </DropdownMenuItem>
