4d5774d49be4f130994c3b4f08507c4e34c5eb3a
Laz <lazgerasimov@gmail.com>
2023-10-26 19:32:50 +0100

    Update Products data table

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -6,15 +6,15 @@ import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { BillboardColumn, columns } from "./columns";
+import { ProductColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 import ApiList from "@/components/ui/api-list";
 
-interface BillboardClientProps {
-    data: BillboardColumn[];
+interface ProductClientProps {
+    data: ProductColumn[];
 }
 
-const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
 
     const router = useRouter();
     const params = useParams();
@@ -23,21 +23,21 @@ const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title={`Billboards (${data.length})`}
-                    description="Manage billboards for your store"
+                    title={`Products (${data.length})`}
+                    description="Manage products for your store"
                 />
-                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
             </div>
             <Separator />
             <DataTable columns={columns} data={data} searchKey="label" />
-            <Heading title="API" description="API calls for Billboards" />
+            <Heading title="API" description="API calls for Products" />
             <Separator />
-            <ApiList entityName="billboards" entityIdName="billboardId" />
+            <ApiList entityName="products" entityIdName="productId" />
         </>
     )
 }
 
-export default BillboardClient;
+export default ProductClient;
