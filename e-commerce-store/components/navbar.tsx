f352d290cac9c3bc69d5add75611ec47e8a6ab9a
Laz <lazgerasimov@gmail.com>
2023-11-01 16:49:18 +0000

    update navbar scaffold

--- e-commerce-store/components/navbar.tsx
+++ e-commerce-store/components/navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Container from '@/components/ui/container';
+import Link from 'next/link';
 
 const Navbar = () => {
     return (
         <div className='border-b'>
-            Navbar
+            <Container>
+                <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center'>
+                    <Link href="/" className='ml-4 flex lg:ml-0 gap-x-2'>
+                        <p className='font-bold text-xl'>STORE</p>
+                    </Link>
+                </div>
+            </Container>
         </div>
     )
 }
