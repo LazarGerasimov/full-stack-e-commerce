5202dd13b483c80b1ad550461feda18c1c4fec44
Laz <lazgerasimov@gmail.com>
2023-11-03 17:14:47 +0000

    Add: - navbar button styles - lucide react icons library (shopping bag) - shopping bag styles

--- e-commerce-store/components/ui/Button.tsx
+++ e-commerce-store/components/ui/Button.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/lib/utils';
+
 import React, { forwardRef } from 'react'
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -13,6 +15,23 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
 }, ref) => {
     return (
         <button
+            className={cn(
+                `
+                w-auto
+                rounded-full
+                bg-black
+                border-transparent
+                px-5
+                py-3
+                disabled:cursor-not-allowed
+                disabled:opacity-50
+                text-white
+                font-semibold
+                hover:opacity-75
+                transition
+                `,
+                className
+            )}
             ref={ref}
         >
             {children}
@@ -21,3 +40,5 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
 });
 
 Button.displayName = "Button";
+
+export default Button;
