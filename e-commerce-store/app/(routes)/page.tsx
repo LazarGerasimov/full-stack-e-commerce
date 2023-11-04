3cdc3f9b9c65f96f985e9c799ed57c1140d243b9
Laz <lazgerasimov@gmail.com>
2023-11-04 15:33:06 +0000

    Update: - billboard comp - add get-billboard comp - mock billboard data - test get billboard call from db

--- e-commerce-store/app/(routes)/page.tsx
+++ e-commerce-store/app/(routes)/page.tsx
@@ -1,11 +1,17 @@
+import getBillboard from "@/actions/get-billboard";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+  const billboard = await getBillboard("f5bb93e1-b2dc-438f-9e91-cb1cfc5a4b90");
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   )
