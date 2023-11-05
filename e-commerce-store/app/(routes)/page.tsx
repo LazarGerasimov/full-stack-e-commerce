2c76b732f78dc41bef9d9f0a0f2cd88516d90e37
Laz <lazgerasimov@gmail.com>
2023-11-05 17:28:21 +0000

    Add: - product-list comp scaffold - add featured products to the landing page

--- e-commerce-store/app/(routes)/page.tsx
+++ e-commerce-store/app/(routes)/page.tsx
@@ -1,17 +1,23 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
 
+  const products = await getProducts({ isFeatured: true }); // just featured products needed for the landing page 
   const billboard = await getBillboard("f5bb93e1-b2dc-438f-9e91-cb1cfc5a4b90");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   )
