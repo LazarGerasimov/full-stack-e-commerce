6d4614a263337bd0a15936c733d998d7ccbcc7c5
Laz <lazgerasimov@gmail.com>
2023-11-07 17:38:33 +0000

    Update product-card: - add product description - add currency component - add product price - update styles

--- e-commerce-store/components/ui/product-card.tsx
+++ e-commerce-store/components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import { Expand, ShoppingCart } from 'lucide-react';
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
 
 interface ProductCard {
     data: Product;
@@ -35,6 +36,19 @@ const ProductCard: React.FC<ProductCard> = ({
                         />
                     </div>
                 </div>
+            </div>
+            {/* Description */}
+            <div>
+                <p className="font-semibold text-lg">
+                    {data.name}
+                </p>
+                <p className="text-sm text-gray-500">
+                    {data.category?.name}
+                </p>
+            </div>
+            {/* Price */}
+            <div className="flex items-center justify-between">
+                <Currency value={data?.price} />
             </div>
         </div>
     )
