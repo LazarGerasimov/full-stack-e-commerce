e14b9180ef659092e502260bee8014d29842beb0
Laz <lazgerasimov@gmail.com>
2023-11-08 18:43:57 +0000

    Add: - get single product api call - update individual product component

--- e-commerce-store/app/(routes)/product/[productId]/page.tsx
+++ e-commerce-store/app/(routes)/product/[productId]/page.tsx
@@ -1,3 +1,6 @@
+import getProduct from "@/actions/get-product";
+import getProducts from "@/actions/get-products";
+import Container from "@/components/ui/container";
 
 
 interface ProductPageProps {
@@ -6,14 +9,29 @@ interface ProductPageProps {
     }
 }
 
-const ProductPage: React.FC<ProductPageProps> = ({
+const ProductPage: React.FC<ProductPageProps> = async ({
     params
 }) => {
-  return (
-    <div>
-        Individual product
-    </div>
-  )
+    const product = await getProduct(params.productId);
+    const suggestedProducts = await getProducts({
+        categoryId: product?.category?.id
+    })
+    return (
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-10 sm:px-6 lg:px-8">
+                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                        {/* Gallery */}
+                        <div>Gallery</div>
+                        <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                            {/* Info */}
+                            Info
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default ProductPage
