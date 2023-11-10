28c818328ff0f0b5ae693c2dd00e60bcef7cf706
Laz <lazgerasimov@gmail.com>
2023-11-10 18:48:12 +0000

    create info component scaffold

---
+++ e-commerce-store/components/info.tsx
@@ -0,0 +1,16 @@
+import { Product } from "@/types"
+
+
+interface InfoProps {
+    data: Product
+}
+
+const Info: React.FC<InfoProps> = ({
+    data
+}) => {
+    return (
+        <div>Info</div>
+    )
+}
+
+export default Info
