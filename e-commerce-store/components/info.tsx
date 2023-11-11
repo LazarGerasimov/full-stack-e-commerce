230be5953c8b7933868f85f2ca44868dcb8c3476
Laz <lazgerasimov@gmail.com>
2023-11-11 16:04:44 +0000

    Update Info component: - add price and size sections & styles

--- e-commerce-store/components/info.tsx
+++ e-commerce-store/components/info.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/types"
+import Currency from "@/components/ui/currency"
 
 
 interface InfoProps {
@@ -9,7 +10,21 @@ const Info: React.FC<InfoProps> = ({
     data
 }) => {
     return (
-        <div>Info</div>
+        <div>
+            <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+            <div className="mt-3 flex items-end justify-between">
+                <p className="text-2xl text-gray-900">
+                    <Currency value={data?.price} />
+                </p>
+            </div>
+            <hr className="my-4" />
+            <div className="flex items-center gap-x-4">
+                <h3 className="font-semibold text-black">Size: </h3>
+                <div>
+                    {data?.size?.name}
+                </div>
+            </div>
+        </div>
     )
 }
 
