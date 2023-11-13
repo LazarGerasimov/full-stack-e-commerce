88dfaff95aecbe08565d86d8efbe014ef670c48b
Laz <lazgerasimov@gmail.com>
2023-11-13 16:20:05 +0200

    add preview modal component

--- e-commerce-store/hooks/use-preview-modal.ts
+++ e-commerce-store/hooks/use-preview-modal.ts
@@ -8,11 +8,11 @@ interface PreviewModalStore {
     onClose: () => void;
 };
 
-const usePreviewModel = create<PreviewModalStore>((set) => ({
+const usePreviewModal = create<PreviewModalStore>((set) => ({
     isOpen: false,
     data: undefined,
     onOpen: (data: Product) => set({ data: data, isOpen: true }),
     onClose: () => set({ isOpen: false })
 }));
 
-export default usePreviewModel;
+export default usePreviewModal;
