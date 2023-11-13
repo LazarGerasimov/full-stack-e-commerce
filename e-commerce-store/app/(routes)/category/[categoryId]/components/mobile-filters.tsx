896fc81903433978b4d0790b708cc3740d6dcbd4
Laz <lazgerasimov@gmail.com>
2023-11-13 15:54:13 +0200

    complete mobile filters

--- e-commerce-store/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ e-commerce-store/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { useState } from 'react';
+import { Plus, X } from 'lucide-react';
+import { Dialog } from '@headlessui/react';
 
 import { Color, Size } from "@/types";
+
 import Button from '@/components/ui/button';
-import { Plus } from 'lucide-react';
+import IconButton from '@/components/ui/icon-button';
+import Filter from './filter';
 
 interface MobileFiltersProps {
     sizes: Size[];
@@ -23,10 +27,41 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({
 
     return (
         <>
-            <Button className='flex items-center gap-x-2 lg:hidden'>
+            <Button onClick={onOpen} className='flex items-center gap-x-2 lg:hidden'>
                 Filters
                 <Plus size={20} />
             </Button>
+
+            <Dialog open={open} as='div' className="relative z-40 lg:hidden" onClose={onClose}>
+                {/* Background */}
+                <div className='fixed inset-0 bg-black bg-opacity-25' />
+
+                {/* Dialog position */}
+                <div className='fixed inset-0 z-40 flex'>
+                    <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+
+                        {/* Close Button */}
+                        <div className='flex items-center justify-end px-4'>
+                            <IconButton icon={<X size={15} onClick={onClose} />} />
+                        </div>
+
+                        {/* Render filters */}
+                        <div className='py-4 ml-3'>
+                            <Filter
+                                valueKey="sizeId"
+                                name="Sizes"
+                                data={sizes}
+                            />
+                            <Filter
+                                valueKey="colorId"
+                                name="Colors"
+                                data={colors}
+                            />
+                        </div>
+                    </Dialog.Panel>
+                </div>
+
+            </Dialog>
         </>
     )
 }
