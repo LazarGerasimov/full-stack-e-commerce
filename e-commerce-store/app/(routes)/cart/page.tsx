6afd5088ec1da1e26355ce26fcd5c604f8f1975e
Laz <lazgerasimov@gmail.com>
2023-11-15 19:18:12 +0200

    update cart view; create cart-item scaffold

--- e-commerce-store/app/(routes)/cart/page.tsx
+++ e-commerce-store/app/(routes)/cart/page.tsx
@@ -2,22 +2,45 @@
 
 import { useEffect, useState } from "react";
 
+import Container from "@/components/ui/container";
+import useCart from "@/hooks/use-cart";
+import CartItem from "./components/cart-item";
+
 const CartPage = () => {
 
-    // prevent hydration error
-    const [isMounted, setIsMounted] = useState(false);
+    // // prevent hydration error
+    // const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(false);
-    }, []);
+    // useEffect(() => {
+    //     setIsMounted(false);
+    // }, []);
 
-    if (!isMounted) {
-        return null;
-    }
+    // if (!isMounted) {
+    //     return null;
+    // }
+
+    const cart = useCart();
 
     return (
-        <div>
-            
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-16 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+                    <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+                        <div className="lg:col-span-7">
+                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
+                            <ul>
+                                {cart.items.map((item) => (
+                                    <CartItem
+                                        key={item.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </div>
     )
 }
