1fbf2f00dd7d924ee8458ff49fb5d8ba3fcaf0bc
Laz <lazgerasimov@gmail.com>
2023-11-19 20:14:24 +0200

    add shadcn dark/light theme toggle

--- admin-dashboard/components/navbar.tsx
+++ admin-dashboard/components/navbar.tsx
@@ -4,6 +4,7 @@ import prismadb from "@/lib/prismadb";
 import { MainNav } from "@/components/main-nav";
 import { StoreSwitcher } from "@/components/store-switcher";
 import { redirect } from "next/navigation";
+import { ModeToggle } from "./theme-toggle";
 
 
 const Navbar = async () => {
@@ -28,6 +29,7 @@ const Navbar = async () => {
                 </div>
                 <MainNav className="mx-6" />
                 <div className="ml-auto flex items-center space-x-4">
+                    <ModeToggle />
                     <UserButton afterSignOutUrl="/" />
                 </div>
             </div>
