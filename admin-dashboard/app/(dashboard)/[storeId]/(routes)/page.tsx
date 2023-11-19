afe9b3e7918afb92c53b061ead7bdd9f15d4f1fd
Laz <lazgerasimov@gmail.com>
2023-11-19 19:37:23 +0200

    create get-total-revenue functionality

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -13,6 +13,10 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
     params
 }) => {
 
+    const totalRevenue = () => {};
+    const salesCount = () => {};
+    const stockCount = () => {};
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
